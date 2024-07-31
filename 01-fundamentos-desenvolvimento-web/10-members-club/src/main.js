56b4c6bd103ad4326fb03c07b5663542afc81b65
Lucas Souza <lucasnando800@hotmail.com>
2024-07-30 22:49:27 -0300

    Add Card de cortes faltante

--- 01-fundamentos-desenvolvimento-web/10-members-club/src/main.js
+++ 01-fundamentos-desenvolvimento-web/10-members-club/src/main.js
@@ -8,6 +8,8 @@ import "./styles/global.css";
 import "./styles/utility.css";
 import "./styles/header-form.css";
 import "./styles/user-card.css";
+import "./styles/cuts-remaining.css";
+import "./styles/fidelity-stamps.css";
 
 // JS
 import "./modules/page-load.js"
