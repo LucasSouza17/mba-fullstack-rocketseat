c9176589ad6eff199a63eb375afa3eb2b2e89c84
Lucas Souza <lucasnando800@hotmail.com>
2024-08-07 21:27:45 -0300

    Criação Post e Sidebar

--- 02-desenvolvimento-web-frontend/01-ignite-feed/src/App.jsx
+++ 02-desenvolvimento-web-frontend/01-ignite-feed/src/App.jsx
@@ -1,4 +1,8 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -6,6 +10,14 @@ export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
