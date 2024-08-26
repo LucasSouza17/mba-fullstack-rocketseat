9916994e321797d6123ccdb761184e77971d4bc4
Lucas Souza <lucasnando800@hotmail.com>
2024-08-26 17:41:27 -0300

    Finalização projeto ignite timer

---
+++ 02-desenvolvimento-web-frontend/02-ignite-timer/src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
